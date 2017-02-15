149513d32f23695dd6a46c2a08c6b6cf8b1f3ce9
Florent Mertens <flomertens@gmail.com>
2017-02-15 14:34:40 +0100

    Add experimental ft_ml_inverse in gridded_inversion

--- config_gridded.py
+++ config_gridded.py
@@ -55,23 +55,57 @@ cg_maxiter = 5000
 # Enable dct sampling
 use_dct = True
 
-dct_fct_r_m0 = util.get_dct2
-dct_fct_i_m0 = util.get_dct2
-dct_fct_r_m1 = util.get_dct2
-dct_fct_i_m1 = util.get_dct2
-
 # You might want to use something like dct_dl = np.ceil(np.pi / out_theta_max)
 # dct_dl = np.ceil(np.pi / (2.* out_theta_max))
 dct_dl = 16
-
-dct_mmax_full_sample = -1
-
-dct_dl_m0 = dct_dl
 
 # using psparse will be faster for ncore > 4, slower otherwise.
 use_psparse = True
 
 compute_alm_noise = True
+
+# =================================================================
+# FT Inversion parameters
+# =================================================================
+
+# ft_inv_nx & ft_inv_ny should preferably be even
+do_ft_inv = False
+ft_inv_nx = 40
+ft_inv_ny = 40
+ft_inv_res = 0.5 * 1 / float(uv_rumax)
+
+
+# =================================================================
+# Post processing
+# =================================================================
+
+# If W=0, odd l+m modes are not recovered but we can estimates them by interpolation
+do_lm_interp = inp_lm_even_only
+
+# even and odd modes are estimated separately, and l-smoothing help getting a converged solution
+# This works by getting ride of high spatial frequency term in l direction. Not needed with do_reduce_fov!
+do_l_smoothing = True
+
+# Save only a sampled set of all lm modes
+do_l_sampling = True
+l_sampling_dl = dct_dl
+l_sampling_lmin = out_lmin
+l_sampling_lmax = out_lmax
+
+# Reduce FoV. Simply convert alm to map, apply a tophat beam and convert back to alm.
+# It is advised to set reduce_fov_lmin & reduce_fov_lmax to the range
+do_reduce_fov = False
+reduce_fov_theta_max = fwhm
+reduce_fov_lmin = out_lmin
+reduce_fov_lmax = out_lmax
+
+# De-apodize
+do_de_apodize = False
+apodize_window_file = ''
+apodize_window_res = 0
+
+do_plot = True
+
 
 # =================================================================
 # General parameters
